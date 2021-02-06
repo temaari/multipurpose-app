9aad0b5737267024f5331a238e2e2df720e7e7f9
temaari <53286793+temaari@users.noreply.github.com>
2021-02-06 22:18:42 +1300

    getting add customer to work in a single page

--- src/views/AddCustomer.vue
+++ src/views/AddCustomer.vue
@@ -3,50 +3,37 @@
 		<h2>Add Another Customer</h2>
 		<v-divider class="pa-2" ></v-divider>
 		<div id="customers" v-for="customer in customers" >
-				<customer :customer="customer" v-model="update" ></customer>
-				<v-btn @click="remove(customer.id)" v-if="customers.length > 1" block >Remove {{customer.id}}</v-btn>
+			<div>
+				<h3>Customer</h3>
 				<v-divider class="pa-2" ></v-divider>
+				<input type="text" name="firstname" placeholder="First Name" v-model="customer['firstName']" />
+				<input type="text" name="lastname" placeholder="Last Name" v-model="customer['lastName']" />
+				<input type="number" name="age" placeholder="Age" v-model="customer['age']" />
+				<input type="text" name="gender" placeholder="Gender" v-model="customer['gender']" />
+			</div>
+			<v-btn @click="remove(customer['id'])" v-if="customers.length > 1" block >Remove</v-btn><br>	
 		</div><br>
 		<v-btn block @click="add()" >Add Customer</v-btn>
-		<div>{{ customers }}</div>
 	</div>
 </template>
 
 <script>
-	import Customer from '../components/AddCustomer/Customer'
 	export default {
 		data() {
 			return {
-				update: {},
 				customers: [],
 			}
 		},
 		mounted() {
 			this.add()
 		},
-		watch: {
-			update: function (data) {
-				this[data['object']].forEach((el) => {
-					if (el.id === data['id']) {
-						el[data['dataName']] = data['value']
-					}
-				})
-			}
-		},
 		methods: {
 			add() {
-				let newCustomer = { id: Date.now() }
-				this.customers.push(newCustomer)
+				this.customers.push({ id: Date.now() })
 			},
 			remove(id) {
 				this.customers = this.customers.filter(customer => customer.id != id)
 			}
 		},
-		components: {
-			Customer: Customer,
-		},
 	}
 </script>
-
-<style scoped>
-</style>
