8dc83716bdaa6c478fdbd72e92ddb256defde947
damari0 <christ@utilmate.com>
2021-02-16 11:04:42 +1300

    finishing touches for input selector

--- src/Components/Inputs/QuickLink.vue
+++ src/Components/Inputs/QuickLink.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h3>Quick Links</h3>
 		<v-divider class="pa-2"></v-divider>
-		<div v-for="(type, i) in types" :key="i"><li><a :href="'#' + i">{{ type }}</a></li></div>
+		<div v-for="(type, i) in types" :key="i"><li><a :href="'#' + i">{{ type['title'] }}</a></li></div>
 		<li><a href="#learn">Learn More</a></li><br>
 	</div>
 </template>
--- src/views/InputSelecter.vue
+++ src/views/InputSelecter.vue
@@ -3,7 +3,7 @@
 		<div><h1>Different types of input</h1><v-divider class="pa-2" ></v-divider></div>
 		<div><p>Select from the different text fields you can use starting with your class html or new vue components like 'v-text-field'</p></div>
 
-		<quick-link :types="types"></quick-link>
+		<quick-link :types="elements"></quick-link>
 
 		<html-element :elements="elements">
 			<template #element_0><input type="text" placeholder="Normal Html Input"/><br><br></template>
@@ -28,25 +28,23 @@
 
 	export default {
 		data: () => ({
-			types: [ 'Basic', 'Normal', 'Outlined', 'Filled', 'Solo', 'Rules', 'Prepend', 'Prefix', 'Suffic', 'Shaped Outlined' ],
 			elements: [
-				{ id: 0, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 1, title: 'Basic Input', html: "<v-text-field label='Normal input' clearable></v-text-field>" },
-				{ id: 2, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 3, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 4, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 5, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 6, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 7, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 8, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
-				{ id: 9, title: 'Basic Input', html: "<input type='text' placeholder='Normal Html Input'/>" },
+				{ id: 0, title: 'Basic', html: "<input type='text' placeholder='Normal Html Input'/>" },
+				{ id: 1, title: 'Normal', html: "<v-text-field label='Normal input' clearable></v-text-field>" },
+				{ id: 2, title: 'Outlined', html: "<v-text-field label='Outlined Input' clearable outlined></v-text-field></template>" },
+				{ id: 3, title: 'Filled', html: "<v-text-field label='Filled Input' clearable filled></v-text-field></template>" },
+				{ id: 4, title: 'Solo', html: "<v-text-field label='Solo Input' clearable solo></v-text-field></template>" },
+				{ id: 5, title: 'rules', html: "<v-text-field label='rules input' clearable :rules='rules' hide-details='auto'></v-text-field></template>" },
+				{ id: 6, title: 'With Icon', html: "<v-text-field label='With Icon' clearable prepend-inner-icon='mdi-map-marker'></v-text-field></template>" },
+				{ id: 7, title: 'Prefix', html: "<v-text-field label='Prefix Input' clearable prefix='$'></v-text-field></template>" },
+				{ id: 8, title: 'Suffix', html: "<v-text-field label='Suffix Input' clearable suffix='@gmail.com'></v-text-field></template>" },
+				{ id: 9, title: 'Shaped outlined', html: "<v-text-field label='Shaped outlined Input' clearable shaped outlined></v-text-field></template>" },
 			],
 			rules: [
 				value => !!value || 'Required.',
 				value => (value && value.length >= 3) || 'Min 3 characters',
 			],
 		}),
-		methods: {},
 		components: {
 			HtmlElement: HtmlElement,
 			QuickLink: QuickLink,
