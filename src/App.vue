992b6e9edd77e8f79cc50d0f863def092d4882c4
damari0 <christ@utilmate.com>
2021-02-18 08:56:11 +1300

    removing my reminder

--- src/App.vue
+++ src/App.vue
@@ -46,10 +46,6 @@
 		</v-app-bar>
 
 		<v-main>
-			<div class="pa-10">
-				<p>Don't forget to create the mall app</p>
-				<p>A place where people come and scroll through stores</p>
-			</div>
 			<router-view></router-view>
 			<v-btn v-if="isHomePage()" block rounded @click="isProd=!isProd">Edit Page</v-btn>
 		</v-main>
