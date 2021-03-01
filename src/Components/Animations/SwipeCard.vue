2d0bf31ac7e93ef142086442770f275f7ff25c3a
temaari <53286793+temaari@users.noreply.github.com>
2021-03-01 17:04:50 +1300

    add swipe card to animation

---
+++ src/Components/Animations/SwipeCard.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<h3>Swipe Card</h3>
+		<v-divider class="pa-2"></v-divider>
+		<div id="container">
+			<div id="card"></div>
+		</div>
+		<div>
+			<button @click="move('left')">left</button>
+			<button @click="move('right')">right</button>
+		</div>
+		<div id="message"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	mounted() {
+	},
+	methods: {
+		move(direction) {
+			let id = null
+			let card = document.getElementById('card')
+			let pos = 75
+			clearInterval(id)
+			setInterval(() => {
+				if (pos == 350 || pos == (-200)) {
+					clearInterval(id)
+				} else {
+					direction === 'left' ? pos-- : pos++
+					card.style.left = pos + "px"
+				}
+			}, 1)
+		},
+	}
+}
+</script>
+
+<style scoped>
+	#container {
+		width: 400px;
+		height: 400px;
+		position: relative;
+		background: rgb(100, 100, 100);
+	}
+	#card {
+		width: 250px;
+		height: 300px;
+		left: 75px;
+		top: 50px;
+		position: absolute;
+		background: rgb(161, 30, 30);
+	}
+	button {
+		padding: 10px;
+		width: 100px;
+		margin-right: 20px;
+		border: 1px solid black;
+	}
+</style>
--- src/views/Animation.vue
+++ src/views/Animation.vue
@@ -3,6 +3,7 @@
 		<h2>JS Animations with CSS</h2>
 		<v-divider class="pa-2"></v-divider>
 		<div><p>This is to showcase my animation learnings with vue and javascript</p></div>
+		<swipe-card></swipe-card><br>
 		<basic-box></basic-box><br>
 		<control-box></control-box>
 	</div>
@@ -11,11 +12,13 @@
 <script>
 import BasicBox from '../components/Animations/BasicBox'
 import ControlBox from '../components/Animations/ControlBox'
+import SwipeCard from '../components/Animations/SwipeCard'
 
 export default {
 	components: {
 		BasicBox,
 		ControlBox,
+		SwipeCard,
 	},
 }
 </script>
