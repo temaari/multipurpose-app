87eb9919cbdf184b22b048ea131eaf1f909377ff
temaari <53286793+temaari@users.noreply.github.com>
2021-03-25 22:54:09 +1300

    login page looks beafutiful

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,26 @@
 <template>
 	<div class="main">
+		<div v-if="!!SaveError" class="error"><strong>Error!</strong> {{ SaveError }}</div>
+		<h3>Account Loggin</h3>
 		<div class="login">
-			<div class="form">
-				<div class="input">
-					<input v-model="username" @keyup.enter="submit()" type="email" name="username" id="username" placeholder="" required/>
-					<label for="username">Username</label>
+			<form class="form">
+				<div class="username">
+					<label class="field input">
+						<input v-model="username" @keyup.enter="submit()" type="text" required :disabled="IsLoading"/>
+						<span class="placeholder">Username</span>
+					</label>
 				</div>
-				<div class="input">
-					<input v-model="password" @keyup.enter="submit()" type="password" name="password" id="password" placeholder="" required/>
-					<label for="password">Password</label>
+				<div class="password">
+					<label class="field input">
+						<input v-model="password" @keyup.enter="submit()" type="password" required :disabled="IsLoading"/>
+						<span class="placeholder">Password</span>
+					</label>
 				</div>
+				<p>&nbsp;</p>
 				<input @click="submit()" class="button" type="button" value="Save"/>
 				<input @click="logout()" class="button" type="button" value="logout"/>
 				<div v-if="IsLoading" class="loader"></div>
-			</div>
-			<div v-if="!!SaveError" class="error"><strong>Error!</strong> {{ SaveError }}</div>
+			</form>
 		</div>
 	</div>
 </template>
@@ -74,11 +80,12 @@ export default {
 				setTimeout(() => {
 					if (this.verifyAccount()) {
 						location.href = location.href + '?env=dev'
-					} else
+						this.IsLoading = false
+					} else {
 						this.SaveError = "Invalid username or password"
-
-					this.IsLoading = false
-				}, 500)
+						this.IsLoading = false
+					}
+				}, 1000)
 			} else
 				this.IsLoading = false
 		},
@@ -95,52 +102,100 @@ export default {
 
 	.main {
 		padding: 2rem;
-		margin: 1.75rem;
 		width: auto;
-		border: 1px solid rgb(160, 170, 160);
+		margin: 3rem 10% 0 10%;
+		border: none;
 	}
 
-	.form .input {
+	.form .field {
+		display: flex;
 		position: relative;
-		overflow: hidden;
+		font-size: 14px;
+		padding-top: 20px;
+		margin-bottom: 5px;
 	}
-	.form label {
-		top: 0;
-		left: 0;
-		position: absolute;
-		display: inline-block;
-		width: 100px;
-		text-align: left;
-		margin: 0 5rem 1.75rem 0;
-	}
-	.form input {
-		position: relative;
-		margin: 0 5rem 1.75rem 0;
-		padding: 0 0 0 0.2rem;
+
+	.form .field input {
+		border: none;
+		-webkit-appearance: none;
+		-ms-appearance: none;
+		-moz-appearance: none;
+		appearance: none;
+		background: #f2f2f2;
+		padding: 12px;
+		border-radius: 3px;
+		width: 250px;
 		outline: none;
-		width: auto;
-		height: 40px;
-		display: inline-block;
-		border-bottom: 0.1rem solid rgb(160, 170, 160);
+		font-size: 14px;
 	}
-	.form input:focus + label {
-		top: 0;
-		color: black;
-		font-size: 60%;
+
+	.form .field .placeholder {
+		position: absolute;
+		left: 12px;
+		top: calc(50% + 10px);
+		transform: translateY(-50%);
+		color: #aaa;
+		transition: 
+			top 0.3s ease,
+			color 0.3s ease,
+			font-size 0.3s ease;
 	}
+
+	.form .field input.dirty + .placeholder,
+	.form .field input:valid + .placeholder,
+	.form .field input:focus + .placeholder {
+		top: 10px;
+		font-size: 10px;
+		color: #222;
+	}
+
+	.form .field.input input {
+		background: none;
+		border: 2px solid #ddd;
+		transition: border-color 0.3s ease;
+	}
+
+	.form .field.input input + .placeholder {
+		left: 8px;
+		padding: 0 5px;
+	}
+
+	.form .field.input input.dirty,
+	.form .field.input input:valid,
+	.form .field.input input:focus {
+		border-color: #222;
+		transition-delay: 0.1s
+	}
+
+	.form .field.input input.dirty + .placeholder,
+	.form .field.input input:valid + .placeholder,
+	.form .field.input input:focus + .placeholder {
+		top: 20px;
+		font-size: 10px;
+		color: #222;
+		background: #fff;
+	}
+
 	.form .button {
+		float: left;
 		border: 0;
 		outline: none;
 		border-radius: 20%;
 		background: rgb(160, 170, 160);
 		padding: 0.35rem;
-		margin-right: 1.75rem;
+		margin: 1.75rem 1.75rem 0 0;
 		width: auto;
 		min-width: 3rem;
+		transition-delay: 0.1s;
+	}
+	.form .button:hover,
+	.form .button:focus {
+		background: rgb(121, 124, 121);
 	}
 
 	.loader {
-		float: right;
+		float: left;
+		position: absolute;
 		border: 4px solid #f3f3f3;
 		border-radius: 50%;
 		border-top: 4px solid #3498db;
@@ -159,5 +214,9 @@ export default {
 	@keyframes spin {
 		0% { transform: rotate(0deg); }
 		100% { transform: rotate(360deg); }
+	}
+
+	.error {
+		height: 2.25rem;
 	}
 </style>
