fbc6d84b0ae7a37317ee737686235f7a38391ce4
damari0 <christ@utilmate.com>
2021-04-28 11:40:54 +1200

    add language setting with json instead of exporting js

--- src/Components/i18n/i18n.js
+++ src/Components/i18n/i18n.js
@@ -1,9 +1,9 @@
 import english from './english'
+import en from './en'
 import _ from 'lodash'
 
 export function i18n(key) {
-	let message = _.get(english, key)
-	if (!message)
-		return key
+	let message = _.get(en, key)
+	if (!message) return key
 	return message
 }
